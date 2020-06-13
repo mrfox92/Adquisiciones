8053d47deb875acb75aea84550a2858a2431aac2
David Alvarez <davidalvarez54@gmail.com>
2020-06-13 18:23:28 -0400

    Agregado template principal

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']  //  añadimos los estilos css
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
